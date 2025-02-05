b636929ce34e32fa9c5ddf0a784b495ddb928627
dustinpike <dustin.pike07@gmail.com>
2025-02-04 19:28:11 -0600

    Build Structure

---
+++ database.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+# TODO Create different databases if not already created
+# Basic Investments
+# Distributions
+# Closed investments for tracking
+# Open investments still making money
+# Future investments?
+
+# TODO Add to databases
+
+# TODO Remove from databases
+
+# TODO Update database entries
